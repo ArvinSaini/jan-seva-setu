f0917cec84e0bd944c982bd8da21e7bd1a62573a
Jeevant Verma <jverma_be23@thapar.edu>
2024-04-21 00:00:45 +0530

    Added more stuff in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,19 @@ const Home = () => {
     const images = [Image1, Image2, Image3];
 
     return (
-        <div className='flex items-start justify-center h-screen'>
-            <ImageCarousel images={images} />
-        </div>
+        <>
+            <div className='flex items-start justify-center h-[611px]'>
+                <ImageCarousel images={images} />
+            </div>
+            <div className='flex flex-row'>
+                <div className='w-1/2 h-[200px] bg-blue-300'>
+                    para1
+                </div>
+                <div className='w-1/2 h-[200px]'>
+                    para2
+                </div>
+            </div>
+        </>
     );
 };
 
